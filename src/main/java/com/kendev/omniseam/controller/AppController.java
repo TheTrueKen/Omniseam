9ecb80bda47a697c75209c6bd95cd8cdc7bbf399
TheTrueKen <81451130+TheTrueKen@users.noreply.github.com>
2022-06-18 23:28:24 -0400

    Update AppController.java
    update 0.02 trying a fix

---
+++ src/main/java/com/kendev/omniseam/controller/AppController.java
@@ -0,0 +1,17 @@
+package com.kendev.omniseam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
+
+}
