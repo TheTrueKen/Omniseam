f74f5d61822bfd2d66a7fa34a1cb03ffd4f05d6f
TheTrueKen <81451130+TheTrueKen@users.noreply.github.com>
2022-07-21 20:23:42 -0400

    Update ProductController.java

--- src/main/java/com/kendev/omniseam/controller/ProductController.java
+++ src/main/java/com/kendev/omniseam/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.kendev.omniseam.controller;
 
 import com.kendev.omniseam.repository.ProductRepository;
+import com.kendev.omniseam.model.Product;
+
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    
+
     private final ProductRepository productRepository;
 
-    public ProductController(ProductRepository productRepository){
+    public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @GetMapping
-    public ResponseEntity getAllProducts(){
+    public ResponseEntity<List<Product>> getAllProducts() {
         return ResponseEntity.ok(this.productRepository.findAll());
     }
 }
